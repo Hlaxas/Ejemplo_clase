8e8f7e5ed5767801cf92edc1743d4e7d4ef13bc8
Hlaxas <miguel.herrera.quintero17@gmail.com>
2025-05-14 16:38:44 -0500

    agregamos tkinter a la visualizacion

--- Analisis.py
+++ Analisis.py
@@ -4,37 +4,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+import io
 
 class DataAnalyzer:
     def __init__(self, df):
         self.df = df
 
     def summary(self):
-        print(self.df.info())
-        print(self.df.describe())
+        buffer = io.StringIO()
+        self.df.info(buf=buffer)
+        salida = buffer.getvalue()
+        salida_describe = self.df.describe().to_string()
+        salida += "\n\n" + salida_describe
+        return salida
+
     def missing_values(self):
         return self.df.isnull()
     def imprimir(self):
       print("Hola")
     
     def correlation_matrix(self):
-        numeric_cols = self.df.select_dtypes(include=[np.number]).columns
-        corr = self.df[numeric_cols].corr()
+        numeric_cols = self.df.select_dtypes(include=[np.number]).columns #para obtener las columnas numéricas
+        corr = self.df[numeric_cols].corr() #para obtener la matriz de correlación
         plt.figure()
-        sns.heatmap(corr, annot=True, cmap='coolwarm')
+        sns.heatmap(corr, annot=True, cmap='coolwarm') #para mostrar la matriz de correlación
         plt.title('Matriz de Correlación')
         plt.show()
     
     def categorical_analisis(self):
-        categorical_cols = self.df.select_dtypes(include=['object']).columns
-        print(f"Columnas categóricas: {categorical_cols}")
+        categorical_cols = self.df.select_dtypes(include=['object']).columns #para obtener las columnas categóricas
+        print(f"Columnas categóricas: {categorical_cols}") 
         column = input("¿Qué columna categórica deseas analizar? ")
         
         if column in categorical_cols:
             plt.figure()
-            sns.countplot(data = self.df, x=column, order=self.df[column].value_counts().index)
+            sns.countplot(data = self.df, x=column, order=self.df[column].value_counts().index) #para contar los valores únicos
             plt.title(f'Conteo de valores en {column}')
-            plt.xticks(rotation=45)
+            plt.xticks(rotation=45) #para rotar las etiquetas del eje x
             plt.show()
         else:
             print(f"La columna {column} no es categórica o no existe en el DataFrame.")
--- visualizacion.py
+++ visualizacion.py
@@ -4,12 +4,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
-
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+from tkinter import messagebox
 from Analisis import DataAnalyzer
 
-data = pd.read_csv('adult.csv')
+data = pd.read_csv('adult.csv') #Puedo colocar la ruta del archivo .csv o de otro archivo .csv
 analizar = DataAnalyzer(data)
+info = analizar.summary()
+def informacion():
+    try:
+        text_area.delete('1.0', tk.END)  # Limpiar el área de texto antes de mostrar la nueva información
+        info = analizar.summary()
+        text_area.insert(tk.END, info)
+    except:
+        messagebox.showerror("Error", "No se pudo obtener la información del DataFrame.")
+        
+        
+ventana = tk.Tk()
+ventana.title("Análisis de Datos")
 
-analizar.summary()
-analizar.correlation_matrix()
-analizar.categorical_analisis()
+boton_summary = tk.Button(ventana, text="Información del DataFrame", command= informacion )
+boton_summary.pack()
+
+text_area = ScrolledText(ventana, width=100, height=30)
+text_area.pack()
+ventana.mainloop()
